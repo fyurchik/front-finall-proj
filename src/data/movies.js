efcb22ef8fa87383680e56d302ee0eb28a363bc2
jyrafenun@gmail.com <jyrafenun@gmail.com>
2025-05-18 17:43:00 +0300

    Add main styles and mocked data for moveis

---
+++ src/data/movies.js
@@ -0,0 +1,50 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Дюна: Частина друга",
+    description: "Продовження епічної саги про Полa Атріда та його подорож пустелею Арракіса.",
+    genre: "Фантастика, Драма",
+    posterUrl: "https://example.com/dune-poster.jpg",
+    showTime: "2023-11-15T19:00:00"
+  },
+  {
+    id: 2,
+    title: "Оппенгеймер",
+    description: "Історія життя фізика-теоретика Дж. Роберта Оппенгеймера, творця атомної бомби.",
+    genre: "Біографія, Драма, Історичний",
+    posterUrl: "https://example.com/oppenheimer-poster.jpg",
+    showTime: "2023-11-15T21:30:00"
+  },
+  {
+    id: 3,
+    title: "Барбі",
+    description: "Барбі виганяють з Барбіленду через недосконалість, і вона вирушає у справжній світ.",
+    genre: "Комедія, Фентезі",
+    posterUrl: "https://example.com/barbie-poster.jpg",
+    showTime: "2023-11-16T18:00:00"
+  },
+  {
+    id: 4,
+    title: "Місія нездійсненна: Розплата",
+    description: "Ітан Хант і його команда знову рятують світ від глобальної загрози.",
+    genre: "Бойовик, Трилер",
+    posterUrl: "https://example.com/mission-impossible-poster.jpg",
+    showTime: "2023-11-16T20:45:00"
+  },
+  {
+    id: 5,
+    title: "Холодний серцем 3",
+    description: "Нова пригода Анни, Ельзи та їхніх друзів у чарівному королівстві Еренделл.",
+    genre: "Мультфільм, Фентезі, Музика",
+    posterUrl: "https://example.com/frozen3-poster.jpg",
+    showTime: "2023-11-17T17:30:00"
+  },
+  {
+    id: 6,
+    title: "Вонка",
+    description: "Молодий Віллі Вонка і його початок у світі шоколадної магії.",
+    genre: "Фентезі, Музика, Сімейний",
+    posterUrl: "https://example.com/wonka-poster.jpg",
+    showTime: "2023-11-17T19:15:00"
+  }
+];
